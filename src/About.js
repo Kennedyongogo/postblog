0f0ae750bcd38ade9c383d8274ea0b4d5aac8874
kennedy ongogo <ongogokennney89@gmail.com>
2024-07-30 16:17:20 +0300

    rty

--- src/About.js
+++ src/About.js
@@ -20,7 +20,7 @@ const About = () => {
     <div className="coordinates">
     <iframe
  src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3989.697389016283!2d34.892306173495825!3d-0.44609933528189155!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMMKwMjYnNDYuMCJTIDM0wrA1Myc0MS42IkU!5e0!3m2!1sen!2ske!4v1722343333043!5m2!1sen!2ske"
- title="A1, Kadongo"
+ title="Carl Car Collection"
   width="600"
   height="450"
   style={{ border: '0' }}  // This is the correct way in JSX
