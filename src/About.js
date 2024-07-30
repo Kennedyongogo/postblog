b24cb9e25deb2ff4a5ebcace057c05d5c3c40bec
kennedy ongogo <ongogokennney89@gmail.com>
2024-07-30 13:51:30 +0300

    march

--- src/About.js
+++ src/About.js
@@ -1,74 +1,9 @@
-import { FaFacebook } from 'react-icons/fa';
-import React from 'react';
-import { useForm, ValidationError } from '@formspree/react';
-
 const About = () => {
-    const facebookUrl = 'https://www.facebook.com/';
-    const [state, handleSubmit] = useForm("mkgwrgyb");
-    if (state.succeeded) {
-        return <p>Thanks for joining!</p>;
-    }
-    return ( 
-        <div>
-            <h1>About Us</h1>
-            <a 
-      href={facebookUrl}
-      target="_blank"
-      rel="noopener noreferrer"
-      style={{
-        textDecoration: 'none', 
-      }}
-    >
-      <FaFacebook 
-        style={{
-          color: '#4267B2', // Facebook blue
-          fontSize: '24px', // Adjust size as needed
-          transition: 'color 0.3s ease',
-        }}
-      />
-    </a>
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="email">
-        Email Address
-      </label>
-      <input
-        id="email"
-        type="email" 
-        name="email"
-        placeholder='Enter Email'
-      />
-      <ValidationError 
-        prefix="Email" 
-        field="email"
-        errors={state.errors}
-      />
-      <input
-        id="phone"
-        type="phone" 
-        name="Phone"
-        placeholder='Enter Phone Number'
-      />
-      <ValidationError 
-        prefix="Phone" 
-        field="phone"
-        errors={state.errors}
-      />
-      <textarea
-        id="message"
-        name="message"
-        placeholder='Type Message'
-      />
-      <ValidationError 
-        prefix="Message" 
-        field="message"
-        errors={state.errors}
-      />
-      <button type="submit" disabled={state.submitting}>
-        Submit
-      </button>
-    </form>
-        </div>
-     );
+    return (
+    <div>
+   <h1>About Us</h1>
+    </div> 
+    );
 }
  
-export default About;
+export default About ;
--- src/Contact.js
+++ src/Contact.js
@@ -1,9 +1,77 @@
+import { FaFacebook } from 'react-icons/fa';
+import React from 'react';
+import { useForm, ValidationError } from '@formspree/react';
+
 const Contact = () => {
-    return (  
+    const facebookUrl = 'https://www.facebook.com/login';
+    const [state, handleSubmit] = useForm("mkgwrgyb");
+    if (state.succeeded) {
+        return <p>Thanks for Contacting Carl Car Collection</p>;
+    }
+    return ( 
         <div>
-            <h1>Contact</h1>
+            <h1>Contact Carl Car Collection</h1>
+            <a 
+      href={facebookUrl}
+      target="_blank"
+      rel="noopener noreferrer"
+      style={{
+        textDecoration: 'none', 
+      }}
+    >
+      <FaFacebook 
+        style={{
+          color: '#4267B2', // Facebook blue
+          fontSize: '24px', // Adjust size as needed
+          transition: 'color 0.3s ease',
+        }}
+      />
+    </a>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="email">
+        Email Address
+      </label>
+      <input
+        id="email"
+        type="email" 
+        name="email"
+        placeholder='Enter Email'
+      />
+      <ValidationError 
+        prefix="Email" 
+        field="email"
+        errors={state.errors}
+      />
+      <label htmlFor="phone">
+        Phone Number
+      </label>
+      <input
+        id="phone"
+        type="phone" 
+        name="Phone"
+        placeholder='Enter Phone Number'
+      />
+      <ValidationError 
+        prefix="Phone" 
+        field="phone"
+        errors={state.errors}
+      />
+      <textarea
+        id="message"
+        name="message"
+        placeholder='Type Message'
+      />
+      <ValidationError 
+        prefix="Message" 
+        field="message"
+        errors={state.errors}
+      />
+      <button type="submit" disabled={state.submitting}>
+        Submit
+      </button>
+    </form>
         </div>
-    );
+     );
 }
  
 export default Contact;
