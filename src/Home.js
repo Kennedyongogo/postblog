4aea21f280075a707dd27abd8b112b794a5ba191
kennedy ongogo <ongogokennney89@gmail.com>
2024-07-30 10:25:05 +0300

    search

--- src/Home.js
+++ src/Home.js
@@ -1,87 +1,76 @@
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import React from 'react';
-const Home = () => {
-    return ( 
-        <div className='cars'>
-        <div className="image-container">
-            <h1>Vintage Cars</h1>
-            < div className="images-wrapper">
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons'; // Importing necessary icons
 
-            <div className="image">
-            <img src="/images/car-4445171_1280.jpg" alt="old car" />
-                <h2>Ford Mustang</h2>
-                <Link to='/car/1'>
-                <button>View More Details</button>
-                </Link>
-            </div>
+// Sample car data
+const cars = [
+  { id: 1, name: 'Ford Mustang', type: 'Vintage', image: '/images/car-4445171_1280.jpg' },
+  { id: 2, name: 'Beetle', type: 'Vintage', image: '/images/vw-beetle-405876_1280.jpg' },
+  { id: 3, name: 'Nissan GTR', type: 'Vintage', image: '/images/opel-5190050_1280.jpg' },
+  { id: 4, name: 'Convertible', type: 'Vintage', image: '/images/oldtimer-1197800_1280.jpg' },
+  { id: 5, name: 'Ferrari', type: 'Sport', image: '/images/ferrari-721858_1280.jpg' },
+  { id: 6, name: 'Lamborghini', type: 'Sport', image: '/images/lamborghini-4815249_1280.jpg' },
+  { id: 7, name: 'BMW', type: 'Sport', image: '/images/bmw-4565066_1280.jpg' },
+  { id: 8, name: 'Porsche', type: 'Sport', image: '/images/porsche-4795517_1280.jpg' }
+];
 
-            <div className="image">
-            <img src="/images/vw-beetle-405876_1280.jpg" alt="old car" />
-                <h2>Beetle</h2>
-                <Link to='/car/2'>
-                <button>View More Details</button>
-                </Link>
-            </div>
+const Services = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value.toLowerCase());
+  };
 
-            <div className="image">
-            <img src="/images/opel-5190050_1280.jpg" alt="old car" />
-                <h2>Nissan GTR</h2>
-                <Link to='/car/3'>
-                <button>View More Details</button>
-                </Link>
-            </div>
+  const filteredCars = cars.filter(car =>
+    car.name.toLowerCase().includes(searchTerm)
+  );
 
-            <div className="image">
-            <img src="/images/oldtimer-1197800_1280.jpg" alt="old car" />
-                <h2>Convertible</h2>
-                <Link to='/car/4'>
-                <button>View More Details</button>
-                </Link>
-            </div>
-            </div>
-            </div>
-            <div className='image-container'>
-
-            <h1>Sport Cars</h1>
-            < div className="images-wrapper">
-
-            <div className="image">
-            <img src="/images/ferrari-721858_1280.jpg" alt="sport car" />
-                <h2>Ferrari</h2>
-                <Link to='/car/5'>
-                <button>View More Details</button>
-                </Link>
-            </div>
-
-            <div className="image">
-            <img src="/images/lamborghini-4815249_1280.jpg" alt="sport car" />
-                <h2>Lamborghini</h2>
-                <Link to='/car/6'>
-                <button>View More Details</button>
-                </Link>
-            </div>
-
-            <div className="image">
-            <img src="/images/bmw-4565066_1280.jpg" alt="sport car" />
-                <h2>BMW</h2>
-                <Link to='/car/7'>
-                <button>View More Details</button>
-                </Link>
-            </div>
-
-            <div className="image">
-            <img src="/images/porsche-4795517_1280.jpg" alt=" car" />
-                <h2>Porsche</h2>
-                <Link to='/car/8'>
-                <button>View More Details</button>
-                </Link>
-            </div>
-            </div>
-            
-
+  return ( 
+    <div className='cars'>
+      <div className='search-container'>
+        <div className='search-icon'>
+          <input 
+            type="text" 
+            placeholder='Search Cars'  
+            value={searchTerm}
+            onChange={handleSearchChange}
+          />
+          <FontAwesomeIcon icon={faSearch} />
         </div>
+      </div>
+      
+      <div className="image-container">
+        <h1>Vintage Cars</h1>
+        <div className="images-wrapper">
+          {filteredCars.filter(car => car.type === 'Vintage').map(car => (
+            <div className="image" key={car.id}>
+              <img src={car.image} alt={car.name} />
+              <h2>{car.name}</h2>
+              <Link to={`/car/${car.id}`}>
+                <button>View More Details</button>
+              </Link>
+            </div>
+          ))}
         </div>
-     );
-}
- 
-export default Home;
+      </div>
+
+      <div className='image-container'>
+        <h1>Sport Cars</h1>
+        <div className="images-wrapper">
+          {filteredCars.filter(car => car.type === 'Sport').map(car => (
+            <div className="image" key={car.id}>
+              <img src={car.image} alt={car.name} />
+              <h2>{car.name}</h2>
+              <Link to={`/car/${car.id}`}>
+                <button>View More Details</button>
+              </Link>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Services;
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faTools, faInfoCircle, faPhone  } from '@fortawesome/free-solid-svg-icons'; // Importing necessary icons
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -21,10 +23,10 @@ const Navbar = () => {
       </div>
       <div className={`body ${isMenuOpen ? 'show' : ''}`}>
         <ul>
-          <li className='active'><a href="/">Home</a></li>
-          <li><a href="/services">Services</a></li>
-          <li><a href="/about">About Us</a></li>
-          <li><a href="/contact">Contact</a></li>
+          <li className='active'><a href="/"><FontAwesomeIcon icon={faHome}/> Home</a></li>
+          <li><a href="/services"><FontAwesomeIcon icon={faTools}/> Services</a></li>
+          <li><a href="/about"><FontAwesomeIcon icon={faInfoCircle}/> About Us</a></li>
+          <li><a href="/contact"><FontAwesomeIcon icon={faPhone}/> Contact</a></li>
         </ul>
       </div>
     </nav>
