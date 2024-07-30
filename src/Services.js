e7c238419a43fde582387d85916ab8e0c831ea21
kennedy ongogo <ongogokennney89@gmail.com>
2024-07-30 11:55:33 +0300

    services

--- src/Services.js
+++ src/Services.js
@@ -1,7 +1,41 @@
 const Services = () => {
     return ( 
-        <div>
+        <div className="service-container">
             <h1>Services</h1>
+            <div className="service-row">
+            <div className="service">
+                <img src="/images/istockphoto-155161419-612x612.jpg" alt="One" />
+                <h2>Car Painting</h2>
+                <p>Restoring a damaged vehicle to its original finish, matching colours, shades, and textures exactly, in a safe and environmentally responsible manner.</p>
+            </div>
+            <div className="service" >
+                <img src="/images/istockphoto-1487963137-612x612.jpg" alt="Two" />
+                <h2>Panel Beating</h2>
+                <p>Removing, repairing and replacing damaged panels, as well as spray painting, chassis alignment, and refitting body hardware.</p>
+            </div>
+            <div className="service" >
+                <img src="/images/istockphoto-598674466-612x612.jpg" alt="Three" />
+                <h2>Engine Overhaul</h2>
+                <p> It is a comprehensive and meticulous process undertaken to rejuvenate and restore the optimal functioning of an internal combustion engine.</p>
+            </div>
+            </div>
+            <div className="service-row">
+            <div className="service" >
+                <img src="/images/car-carrier-trailer-6839728_1280.jpg" alt="Four" />
+                <h2>Car Shipping</h2>
+                <p>The act of moving a car from one location to another using a specialized trailer.</p>
+            </div>
+            <div className="service" >
+                <img src="/images/icon-8794676_1280.jpg" alt="Five" />
+                <h2>Car Tracking</h2>
+                <p>The system that allows tracking and controlling of vehicles via an online computer, smart phone, tablet, etc. on a 24/7 basis thanks to GPS satellites.</p>
+            </div>
+            <div className="service" >
+                <img src="/images/istockphoto-1413498884-612x612.jpg" alt="Six" />
+                <h2>Car Collection</h2>
+                <p>this is collection of various cars</p>
+            </div>
+            </div>
         </div>
      );
 }
